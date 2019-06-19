05a2a7f4fb35075c64639e7b415a58fd0064f8f5
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-06-19 22:32:45 +0300

    Isolate the benchmarks to prove that the results change

--- benchmarks/Unfoldl.elm
+++ benchmarks/Unfoldl.elm
@@ -31,44 +31,4 @@ main =
                   "List" (\ _ -> List.map op (List.map op (List.map op (List.map op sample))))
                   "Unfoldl" (\ _ -> Unfoldl.toList (Unfoldl.map op (Unfoldl.map op (Unfoldl.map op (Unfoldl.map op (Unfoldl.list sample))))))
               ]
-        ,
-        describe "concat" <|
-          let
-            bySize size =
-              let
-                sample = List.range 0 100 |> List.repeat size
-                in
-                  Benchmark.compare (String.fromInt size)
-                    "List" (\ _ -> List.concat sample)
-                    "Unfoldl" (\ _ -> Unfoldl.toList (Unfoldl.joinMap Unfoldl.list (Unfoldl.list sample)))
-            in
-              [
-                bySize 10
-              ]
-        ,
-        let
-          sample = List.range 0 100 |> List.repeat 10
-          op x = x + 1
-          in
-            Benchmark.compare "concatMap & map"
-              "List" (\ _ -> List.concatMap (List.map op) sample)
-              "Unfoldl" (\ _ -> Unfoldl.toList (Unfoldl.joinMap (Unfoldl.map op << Unfoldl.list) (Unfoldl.list sample)))
-        ,
-        describe "append" <|
-          let
-            sample = List.range 0 100
-            in
-              [
-                Benchmark.compare "single"
-                  "List" (\ _ -> sample ++ sample)
-                  "Unfoldl" (\ _ -> Unfoldl.toList (Unfoldl.prepend (Unfoldl.list sample) (Unfoldl.list sample)))
-                ,
-                Benchmark.compare "double"
-                  "List" (\ _ -> sample ++ sample ++ sample)
-                  "Unfoldl" (\ _ -> Unfoldl.toList (Unfoldl.prepend (Unfoldl.list sample) (Unfoldl.prepend (Unfoldl.list sample) (Unfoldl.list sample))))
-                ,
-                Benchmark.compare "triple"
-                  "List" (\ _ -> sample ++ sample ++ sample ++ sample)
-                  "Unfoldl" (\ _ -> Unfoldl.toList (Unfoldl.prepend (Unfoldl.list sample) (Unfoldl.prepend (Unfoldl.list sample) (Unfoldl.prepend (Unfoldl.list sample) (Unfoldl.list sample)))))
-              ]
       ]
