aa2a795e2bf5ec54c9a9902287bf977d4e4fcaa7
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-06-22 12:24:49 +0300

    Add more benchmarks

--- benchmarks/Unfoldl.elm
+++ benchmarks/Unfoldl.elm
@@ -97,4 +97,29 @@ main =
             in comparison
               "List" (\ _ -> Set.fromList (List.concatMap Array.toList sample))
               "Unfoldl" (\ _ -> Unfoldl.toSet (Unfoldl.joinMap Unfoldl.array (Unfoldl.list sample)))
+        ,
+        define "map and concat arrays into set" <|
+          let
+            sample = List.repeat 10 (Array.fromList (List.range 0 100))
+            op x = x + 1
+            in comparison
+              "List" (\ _ -> Set.fromList (List.concatMap (List.map op << Array.toList) sample))
+              "Unfoldl" (\ _ -> Unfoldl.toSet (Unfoldl.joinMap (Unfoldl.map op << Unfoldl.array) (Unfoldl.list sample)))
+        ,
+        define "map and concat arrays into list regardless of order" <|
+          let
+            sample = List.repeat 10 (Array.fromList (List.range 0 100))
+            op x = x + 1
+            in comparison
+              "List" (\ _ -> (List.concatMap (List.map op << Array.toList) sample))
+              "Unfoldl" (\ _ -> Unfoldl.toReverseList (Unfoldl.joinMap (Unfoldl.map op << Unfoldl.array) (Unfoldl.list sample)))
+        ,
+        define "concat different datastructures into list regardless of order" <|
+          let
+            list = List.range 0 99
+            array = Array.fromList (List.range 100 199)
+            set = Set.fromList (List.range 200 299)
+            in comparison
+              "List" (\ _ -> List.concat [list, Array.toList array, Set.toList set])
+              "Unfoldl" (\ _ -> Unfoldl.toReverseList (Unfoldl.concat [Unfoldl.list list, Unfoldl.array array, Unfoldl.set set]))
       ]
